29a2d0b43f10367aa12ec1839074a458165aaddd
Dan Wagner <danwagnerco@gmail.com>
2014-08-04 23:13:23 -0500

    finish day 3 sign in part 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get "signup" => "users#new"
   resources :users
+  get "signin" => "sessions#new"
+  resource :session
 
   root "movies#index"
   resources :movies do
