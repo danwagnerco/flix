350b6eaff971beded8670d4873c2c0e5cd4c8879
Dan Wagner <danwagnerco@gmail.com>
2014-08-05 22:36:49 -0500

    finish day 4 sign in part 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,12 @@ class User < ActiveRecord::Base
   validates :username, :presence => true,
                        :uniqueness => {:case_sensitive => false}
 
+  def self.authenticate(email_or_username, password)
+    user = User.find_by(:email => email_or_username) || 
+           User.find_by(:username => email_or_username) 
+    user && user.authenticate(password)
+  end
+
   def gravatar_id
     Digest::MD5::hexdigest(email.downcase)
   end
--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -2,19 +2,40 @@ require_relative("../spec_helper")
 
 describe "User sign in" do
 
-	it "shows the sign in form" do
-		User.create!(user_attributes)
+	it "shows the users profile page on success" do
+		u = User.create!(user_attributes)
 		visit root_url
-		click_on "Sign In"
+		click_link "Sign In"
 
 		expect(current_path).to eql(signin_path)
 		expect(page).to have_text("Email")
 		expect(page).to have_text("Password")
 
-		# fill_in "Email",    :with => "user@example.com"
-		# fill_in "Password", :with => "secret"
+		fill_in "Email",    :with => u.email
+		fill_in "Password", :with => u.password
 
-		# click_on "Log In"
+		click_button "Sign In"
+
+		expect(current_path).to eql(user_path(u))
+		expect(page).to have_text("Welcome back, #{u.name}!")
+	end
+
+	it "redirects a bad sign in" do
+		u = User.create!(user_attributes)
+		visit root_url
+		click_link "Sign In"
+
+		expect(current_path).to eql(signin_path)
+		expect(page).to have_text("Email")
+		expect(page).to have_text("Password")
+
+		fill_in "Email",    :with => u.email
+		fill_in "Password", :with => "wrong"
+
+		click_button "Sign In"
+
+		# expect(current_path).to eql(signin_path)
+		expect(page).to have_text("Invalid email/password combination!")
 	end
 
 end
