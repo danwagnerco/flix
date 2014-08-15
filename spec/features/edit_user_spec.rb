3d560e63cf6cd4e6ca1a63fa88aacddaed1fd888
Dan Wagner <danwagnerco@gmail.com>
2014-08-15 06:04:24 -0500

    finish day 9 authorization part 2

--- spec/features/edit_user_spec.rb
+++ spec/features/edit_user_spec.rb
@@ -1,11 +1,8 @@
 require_relative("../spec_helper")
 
 describe "Editing a user" do
-	
-	before(:all) do
-	end
 
-	it "Changes a users name" do
+	it "Changes a newly-created users name" do
 		visit signup_url
 		
 		fill_in "Name", 						:with => "Test"
@@ -27,4 +24,15 @@ describe "Editing a user" do
 		expect(page).to have_text("Account successfully updated!")
 	end
 
+	it "Logs in and changes a users email" do
+		u = User.create!(user_attributes)
+		sign_in(u)
+
+		click_on "Edit Account"
+		fill_in "Email", :with => "shrimp@example.com"
+		click_on "Update Account"
+
+		expect(page).to have_text("shrimp@example.com")
+		expect(page).to have_text("Account successfully updated!")
+	end
 end
